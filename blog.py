0613973b8d7b1c277ad039fe6fe255ed792bdcce
Mustafa <76392495+sencanmustafa@users.noreply.github.com>
2021-08-26 21:11:28 +0300

    upload

--- blog.py
+++ blog.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 from flask import Flask , render_template ,flash,redirect,url_for,session,logging,request
 from wtforms import Form,StringField,TextAreaField,PasswordField,validators
 from passlib.hash import sha256_crypt
